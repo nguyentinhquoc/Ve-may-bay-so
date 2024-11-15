cf31ad16aaf9fdb0df92c8418a481e29bb4a6223
Nguyen huu phu <phub3@gmail.com>
2024-11-15 22:56:09 +0700

    create ui page flight, update next previus

---
+++ assets/js/flight.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+    $('.faq-item').click(function(){
+        // Toggle answer
+        $(this).find('.faq-answer').slideToggle();
+
+        // Toggle arrow direction
+        $(this).find('.arrow').toggleClass('down up');
+    });
+});
+
+
+$(document).ready(function() {
+    var $agentList = $(".agent-list-content_1");
+    var $cards = $(".item_daily_1");
+    var totalItems = $cards.length;
+    var itemsToShow = 4;
+    var currentIndex = 0;
+  
+    var $prevButton = $("#prevButton_1");
+    var $nextButton = $("#nextButton_1");
+  
+    function updateSlides() {
+      const translateX = -(currentIndex * (100 / itemsToShow));
+      $agentList.css('transform', `translateX(${translateX}%)`);
+  
+      $prevButton.prop('disabled', currentIndex === 0);
+      $nextButton.prop('disabled', currentIndex + itemsToShow >= totalItems);
+    }
+  
+    $nextButton.click(function() {
+      if (currentIndex + itemsToShow < totalItems) {
+        currentIndex++;
+        updateSlides();
+      }
+    });
+  
+    $prevButton.click(function() {
+      if (currentIndex > 0) {
+        currentIndex--;
+        updateSlides();
+      }
+    });
+  
+    updateSlides();
+  });
+  
--- assets/js/main.js
+++ assets/js/main.js
@@ -21,3 +21,38 @@ document.addEventListener('DOMContentLoaded', function () {
     $('.btn-new-application').removeClass('active');
   });
 });
+
+$(document).ready(function() {
+  var $agentList = $(".agent-list-content");
+  var $cards = $(".item_daily");
+  var totalItems = $cards.length;
+  var itemsToShow = 7;
+  var currentIndex = 0;
+
+  var $prevButton = $("#prevButton");
+  var $nextButton = $("#nextButton");
+
+  function updateSlides() {
+    const translateX = -(currentIndex * (100 / itemsToShow));
+    $agentList.css('transform', `translateX(${translateX}%)`);
+
+    $prevButton.prop('disabled', currentIndex === 0);
+    $nextButton.prop('disabled', currentIndex + itemsToShow >= totalItems);
+  }
+
+  $nextButton.click(function() {
+    if (currentIndex + itemsToShow < totalItems) {
+      currentIndex++;
+      updateSlides();
+    }
+  });
+
+  $prevButton.click(function() {
+    if (currentIndex > 0) {
+      currentIndex--;
+      updateSlides();
+    }
+  });
+
+  updateSlides();
+});
